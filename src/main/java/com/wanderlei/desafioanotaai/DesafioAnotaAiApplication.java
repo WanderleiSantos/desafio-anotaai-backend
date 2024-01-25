2002e3372298f00e295ba32072b3a31c4df994c5
Wanderlei Santos <wanderleisantosdias@gmail.com>
2024-01-25 15:01:14 -0300

    Initial Commit

---
+++ src/main/java/com/wanderlei/desafioanotaai/DesafioAnotaAiApplication.java
@@ -0,0 +1,13 @@
+package com.wanderlei.desafioanotaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAnotaAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioAnotaAiApplication.class, args);
+    }
+
+}
