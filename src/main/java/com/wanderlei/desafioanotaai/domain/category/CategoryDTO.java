7de936e72897d8a3a5edde8b48dbecebe7ab0b77
Wanderlei Santos <wanderleisantosdias@gmail.com>
2024-02-02 11:32:46 -0300

    Created CategoryController

---
+++ src/main/java/com/wanderlei/desafioanotaai/domain/category/CategoryDTO.java
@@ -0,0 +1,4 @@
+package com.wanderlei.desafioanotaai.domain.category;
+
+public record CategoryDTO(String title, String description, String ownerId) {
+}
---
+++ src/main/java/com/wanderlei/desafioanotaai/services/CategoryService.java
@@ -0,0 +1,45 @@
+package com.wanderlei.desafioanotaai.services;
+
+import com.wanderlei.desafioanotaai.domain.category.Category;
+import com.wanderlei.desafioanotaai.domain.category.CategoryDTO;
+import com.wanderlei.desafioanotaai.domain.category.exceptions.CategoryNotFoundException;
+import com.wanderlei.desafioanotaai.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    private CategoryRepository repository;
+
+    public CategoryService(CategoryRepository repository) {
+        this.repository = repository;
+    }
+
+    public Category insert(CategoryDTO categoryData) {
+        Category newCategory = new Category(categoryData);
+        this.repository.save(newCategory);
+        return newCategory;
+    }
+
+    public Category update(String id, CategoryDTO categoryData) {
+        Category category = this.repository.findById(id).orElseThrow(CategoryNotFoundException::new);
+
+        if (!categoryData.title().isEmpty()) category.setTitle(categoryData.title());
+        if (!categoryData.description().isEmpty()) category.setDescription(categoryData.description());
+
+        this.repository.save(category);
+        return category;
+    }
+
+    public void delete(String id) {
+        Category category = this.repository.findById(id).orElseThrow(CategoryNotFoundException::new);
+        this.repository.delete(category);
+    }
+
+    public List<Category> getAll() {
+        return this.repository.findAll();
+    }
+
+
+}
